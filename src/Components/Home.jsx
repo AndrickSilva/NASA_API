4da0f4dd0b0f383f342cf0076b1a207791a50516
AndrickSilva <silva.andrick2001@gmail.com>
2023-02-08 12:47:54 +0530

    responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -16,9 +16,9 @@ const Home = () => {
     return (
         <div className='bg-slate-800 flex justify-center items-center flex-col h-screen'>
             <img src={Photo?.hdurl} alt={Photo?.title} className='h-72 shadow-2xl' />
-            <h1 className='font-bold text-slate-200 my-5 text-3xl'>{Photo?.title}</h1>
+            <h1 className='font-bold text-slate-200 my-5 text-xl lg:text-3xl'>{Photo?.title}</h1>
             <p className='mb-5 text-cyan-500 font-bold'>{Photo?.date}</p>
-            <p className='w-full px-2 lg:w-1/2 mx-auto text-slate-300 text-center'>{Photo?.explanation}</p>
+            <p className='w-full px-2 lg:w-1/2 mx-auto text-slate-300 text-justify'>{Photo?.explanation}</p>
             <div className='text-cyan-500 bg-slate-900 w-full text-center py-3 mt-10'>© | {Photo?.copyright}</div>
         </div>
     )
