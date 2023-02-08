5b99497b99ae98ac3f3bbd81fb60cb06376a2a1a
AndrickSilva <silva.andrick2001@gmail.com>
2023-02-08 12:44:47 +0530

    completed styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,25 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
     const [Photo, setPhoto] = useState(null)
     useEffect(() => {
         fetchPhoto()
-        async function fetchPhoto () {
+        async function fetchPhoto() {
             const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=jbaQhmnXsZHf3ACLHQBOXAu5S9wte0hfHaGsGN3T`)
             const data = await res.json();
             setPhoto(data)
         };
-        
+
     }, [])
     console.log(Photo);
-    
+
     return (
-        <div>
-                <img src={Photo?.hdurl} alt={Photo?.title} className='h-60' />
-                <h1 className='font-bold'>{Photo?.title}</h1>
-                <p>{Photo?.date}</p>
-                <p>{Photo?.explanation}</p>
-                <p>{Photo?.copyright}</p>
+        <div className='bg-slate-800 flex justify-center items-center flex-col h-screen'>
+            <img src={Photo?.hdurl} alt={Photo?.title} className='h-72 shadow-2xl' />
+            <h1 className='font-bold text-slate-200 my-5 text-3xl'>{Photo?.title}</h1>
+            <p className='mb-5 text-cyan-500 font-bold'>{Photo?.date}</p>
+            <p className='w-full px-2 lg:w-1/2 mx-auto text-slate-300 text-center'>{Photo?.explanation}</p>
+            <div className='text-cyan-500 bg-slate-900 w-full text-center py-3 mt-10'>© | {Photo?.copyright}</div>
         </div>
     )
 }
